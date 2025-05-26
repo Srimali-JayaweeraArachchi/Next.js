2680fd94b1c3d206e51d73a1f59ca9a7ede44a80
Srimali <jasrimalimadurangani@gmail.com>
2025-05-26 12:23:44 +0530

    Create socials,carriers,branches page

---
+++ myfirstnextapp/src/app/about/branches/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      Branches
+    </div>
+  )
+}
+
+export default page
---
+++ myfirstnextapp/src/app/about/socials/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      Socials
+    </div>
+  )
+}
+
+export default page
