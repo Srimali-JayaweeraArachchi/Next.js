a9f90dc301faf50f41c137178138743e18fe39f3
Srimali <jasrimalimadurangani@gmail.com>
2025-05-26 20:49:38 +0530

    css styling

--- myfirstnextapp/src/app/contact/page.jsx
+++ myfirstnextapp/src/app/contact/page.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
+import styles from './contact.module.css'
 
 function page() {
   return (
     <> 
     <div>
       <h1>Contact us page</h1>
-       <Link href="/">Back to home</Link>
+       <Link href="/" className={styles.link}>Back to home</Link>
     </div>
     </>
     
--- myfirstnextapp/src/app/page.jsx
+++ myfirstnextapp/src/app/page.jsx
@@ -6,16 +6,16 @@ export default function Home() {
   return (
     <><h1>Home Page</h1>
     <ul>
-      <li><Link href="/"> Home</Link></li>
-      <li><Link href="/services"> Services</Link></li>
-      <li><Link href="/products"> Products</Link></li>
-      <li><Link href="/about"> About</Link></li>
-      <li><Link href="/contact"> Contact us</Link></li>
-      <li><Link href="/about/branches"> Branches</Link></li>
-       <li><Link href="/products/vegis"> Vegis</Link></li>
+      <li><Link href="/" className={styles.link}> Home</Link></li>
+      <li><Link href="/services" className={styles.link}> Services</Link></li>
+      <li><Link href="/products" className={styles.link}> Products</Link></li>
+      <li><Link href="/about" className={styles.link}> About</Link></li>
+      <li><Link href="/contact" className={styles.link}> Contact us</Link></li>
+      <li><Link href="/about/branches" className={styles.link}> Branches</Link></li>
+       <li><Link href="/products/vegis" className={styles.link}> Vegis</Link></li>
     </ul>
 
-    <Link href="https://nextjs.org/"target="_blank">NextJS Site</Link>
+    <Link href="https://nextjs.org/"target="_blank" id={styles.specialLink}>NextJS Site</Link>
     
     </>
     
