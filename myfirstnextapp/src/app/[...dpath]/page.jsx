62c55c97a959fbd8583e773458a90c1eb8b94d49
Srimali <jasrimalimadurangani@gmail.com>
2025-05-26 14:44:15 +0530

    Catch all routes

--- myfirstnextapp/src/app/[...dpath]/page.jsx
+++ myfirstnextapp/src/app/[...dpath]/page.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
-function page() {
+function page({params}) {
+    console.log(params)
   return (
+    <>
     <div>
       Catch all route page
     </div>
+    <ul>
+        {params.dpath.map((item) => {
+        return (
+            <li>{item}</li>
+        )
+    })}
+    </ul>
+</>
   )
 }
 
